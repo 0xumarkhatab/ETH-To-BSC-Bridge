4473a47c56ccf4c50eefcd11745128c1e0503e3f
umaresso <umarkhatab467@gmail.com>
2023-01-29 15:25:50 +0500

    Added Scripts for the bridge

--- scripts/eth-bsc-bridge.js
+++ scripts/eth-bsc-bridge.js
@@ -2,35 +2,51 @@ const Web3 = require("web3");
 const BridgeEth = require("../build/contracts/BridgeEth.json");
 const BridgeBsc = require("../build/contracts/BridgeBsc.json");
 
+// Instantiating web3 object with the Ethereum network's WebSocket URL
 const web3Eth = new Web3(
   "wss://goerli.infura.io/ws/v3/685daa6fa7f94b4b89cdc6d7c5a8639e"
 );
-const web3Bsc = new Web3("https://data-seed-prebsc-1-s1.binance.org:8545");
+
+// Instantiating web3 object with the Binance Smart Chain network's RPC URL
+const web3Bsc = new Web3("https://data-seed-prebsc-1-s3.binance.org:8545");
+
+// The private key of the wallet to be used as the admin address
 const adminPrivKey =
   "297b88a79adffda6a5e37e1cc34da8a29cd60d14f66c15f21006f39e0d69540a";
+
+// Deriving the public address of the wallet using the private key
 const { address: admin } = web3Bsc.eth.accounts.wallet.add(adminPrivKey);
 
+// Instantiating the BridgeEth contract with its ABI and address
 const bridgeEth = new web3Eth.eth.Contract(
   BridgeEth.abi,
   BridgeEth.networks["5"].address
 );
 
+// Instantiating the BridgeBsc contract with its ABI and address
 const bridgeBsc = new web3Bsc.eth.Contract(
   BridgeBsc.abi,
   BridgeBsc.networks["97"].address
 );
 
+// Listening to Transfer events emitted by the BridgeEth contract
 bridgeEth.events
   .Transfer({ fromBlock: 0, step: 0 })
   .on("data", async (event) => {
-    const { from, to, amount, date, nonce, signature } = event.returnValues;
-
+    // Destructuring the values from the event
+    const { from, to, amount, date, nonce, signature } = event.returnValues; // Defining the method to be called on the BridgeBsc contract
     const tx = bridgeBsc.methods.mint(from, to, amount, nonce, signature);
+
+    // Getting the gas price and gas cost required for the method call
     const [gasPrice, gasCost] = await Promise.all([
       web3Bsc.eth.getGasPrice(),
       tx.estimateGas({ from: admin }),
     ]);
+
+    // Encoding the ABI of the method
     const data = tx.encodeABI();
+
+    // Preparing the transaction data
     const txData = {
       from: admin,
       to: bridgeBsc.options.address,
@@ -38,14 +54,20 @@ bridgeEth.events
       gas: gasCost,
       gasPrice,
     };
+
+    // Sending the transaction to the Binance Smart Chain
     const receipt = await web3Bsc.eth.sendTransaction(txData);
+
+    // Logging the transaction hash
     console.log(`Transaction hash: ${receipt.transactionHash}`);
+
+    // Logging the details of the processed transfer
     console.log(`
-    Processed transfer:
-    - from ${from} 
-    - to ${to} 
-    - amount ${amount} tokens
-    - date ${date}
-    - nonce ${nonce}
-  `);
+Processed transfer:
+- from ${from} 
+- to ${to} 
+- amount ${amount} tokens
+- date ${date}
+- nonce ${nonce}
+`);
   });
--- truffle-config.js
+++ truffle-config.js
@@ -1,55 +1,9 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * https://trufflesuite.com/docs/truffle/reference/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// require('dotenv').config();
-// const mnemonic = process.env["MNEMONIC"];
-// const infuraProjectId = process.env["INFURA_PROJECT_ID"];
-
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic =
   "dawn rent you scissors south abuse once violin unveil birth slice noise";
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
-
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache, geth, or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    //
     ethereum_testnet: {
       provider: () =>
         new HDWalletProvider(
@@ -66,7 +20,7 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(
           mnemonic,
-          `https://data-seed-prebsc-1-s3.binance.org:8545/`
+          `https://data-seed-prebsc-1-s3.binance.org:8545`
         ),
       skipDryRun: true,
       network_id: 97, // Binance Snart Chain testnet's id
